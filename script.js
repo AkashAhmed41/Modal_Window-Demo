094d78f61fafb1505e00c2047c94f37a5064c59e
Md. Akash Ahmed <86203010+AkashAhmed41@users.noreply.github.com>
2022-11-10 02:55:30 +0600

    js code refactored

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
+//Declaring functions for reafctoring...
+
 const showModalWindow = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -17,13 +19,31 @@ const closeModalWindow = function () {
 
 for (let i = 0; i < btnsShowModal.length; i++) {
   btnsShowModal[i].addEventListener('click', showModalWindow);
+  //                                    function () {
+  //   modal.classList.remove('hidden');
+  //   overlay.classList.remove('hidden');
+  // });
 }
 
 btnCloseModal.addEventListener('click', closeModalWindow);
+//                                    function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
 
 overlay.addEventListener('click', closeModalWindow);
+//                                    function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
 
 document.addEventListener('keydown', function (e) {
+  // if (e.key === 'Escape') {
+  //   if (!modal.classList.contains('hidden')) {
+  //     closeModalWindow();
+  //   }
+  // }
+
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModalWindow();
   }
